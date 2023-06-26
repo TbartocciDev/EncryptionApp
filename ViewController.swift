3bab16039c7022d606eb2bffc2f747fff1f6e0f7
TbartocciDev <tommybartocci@gmail.com>
2023-06-26 11:37:01 -0400

    initialized project

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EncryptionApp
+//
+//  Created by Tommy Bartocci on 6/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
